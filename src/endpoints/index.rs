b3ca0478d84fd1ab8bd5f8e8df4f5503f965567f
Stefan Netterfield <stefan@5f03.com>
2023-09-01 02:12:02 -0400

    Got hello world running

---
+++ src/endpoints/index.rs
@@ -0,0 +1,23 @@
+use actix_web::{web, HttpRequest, Responder};
+use serde::Serialize;
+use sqlx::{postgres::PgRow, Row};
+use crate::AppState;
+
+
+#[derive(Serialize)]
+pub struct HelloWorld {
+    code: i32,
+    message: String,
+    db_tables: Vec<String>
+}
+
+pub async fn index(request: HttpRequest) -> impl Responder {
+    let app_state: &web::Data<AppState> = request.app_data().expect("App state is missing!");
+    let query_result = sqlx::query("SELECT (table_name) FROM information_schema.tables;")
+        .map(|item: PgRow| item.get("table_name"))
+        .fetch_all(&app_state.db)
+        .await
+        .unwrap();
+
+    return web::Json(HelloWorld { code: 200, message: "Hello, World!".to_string(), db_tables: query_result});
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use sqlx::postgres::{PgPool, PgPoolOptions};
+use dotenv::dotenv;
+use std::env;
+mod endpoints;
+mod db;
+
+pub struct AppState {
+    db: PgPool
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let host: String = env::var("HOST").expect("Host IP to run api on is required!");
+    let port: i32 = env::var("PORT").expect("Port number to run api on is requires!").parse().unwrap();
+    let db_uri: String = env::var("DB_URI").expect("A database URI is required!");
+    let pool = match PgPoolOptions::new()
+       .max_connections(5)
+       .connect(&db_uri)
+       .await
+       {
+        Ok(pool) => {
+            println!("Connected to the database successfully!");
+            pool
+        }
+        Err(error) => {
+            println!("Failed to connect to the database: {:?}", error);
+            std::process::exit(1);
+        }
+       };
+
+    HttpServer::new(move || {
+        let app = App::new()
+            .app_data(web::Data::new(AppState{ db: pool.clone()}))
+            .configure(endpoints::endpoints_factory);
+
+        return app;
+    })
+    .bind(format!("{url_host}:{url_port}", url_host = &host, url_port = &port))?
+    .run()
+    .await
 }
